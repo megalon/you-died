44de4cc0a4bdc1a8cb8a7fa1c4f4097936fe3342
megalon <megalongaming@gmail.com>
2024-10-19 05:19:34 -0400

    Test fading in and out text

--- you-died/MainWindow.xaml.cs
+++ you-died/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,62 @@ namespace you_died
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DateTime _lastFrameTime;
+
+        private double _targetOpacity;
+        private double _lerpAmount;
+        private const double _maxOpacity = 0.6;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Debug.WriteLine("hello testing");
+
+            _lastFrameTime = DateTime.Now;
+
+            Visibility = Visibility.Visible;
+
+            CompositionTarget.Rendering += Update;
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            DateTime currentFrameTime = DateTime.Now;
+
+            double deltaTime = (currentFrameTime - _lastFrameTime).TotalSeconds;
+
+            UpdateMainOpacity(deltaTime);
+
+            _lastFrameTime = currentFrameTime;
+        }
+
+        private void UpdateMainOpacity(double deltaTime)
+        {
+            _lerpAmount += deltaTime;
+            Main.Opacity = double.Lerp(Main.Opacity, _targetOpacity, _lerpAmount);
+
+
+            if (Main.Opacity <= 0)
+            {
+                _lerpAmount = 0;
+                _targetOpacity = _maxOpacity;
+            }
+
+            if (Main.Opacity >= _maxOpacity)
+            {
+                _lerpAmount = 0;
+                _targetOpacity = 0;
+            }
+        }
+
+        private async Task Delay()
+        {
+            Debug.WriteLine("Waiting....");
+            await Task.Delay(1000);
+            Debug.WriteLine("Done waiting!");
+
+            Visibility = Visibility.Visible;
         }
     }
 }
