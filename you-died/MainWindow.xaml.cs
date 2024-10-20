21774e3ae634a1d9bd6853d7b0492c1a937c4f0f
megalon <megalongaming@gmail.com>
2024-10-19 20:49:41 -0400

    Monitor all processes on launch. Show process name in message

--- you-died/MainWindow.xaml.cs
+++ you-died/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,17 +24,63 @@ namespace you_died
         private double _lerpAmount;
         private const double _maxOpacity = 0.6;
 
+        private Process[] _processesToMonitor;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Debug.WriteLine("hello testing");
-
             _lastFrameTime = DateTime.Now;
 
-            Visibility = Visibility.Visible;
+            Visibility = Visibility.Hidden;
 
             CompositionTarget.Rendering += Update;
+
+            _processesToMonitor = Process.GetProcesses();
+
+            foreach (Process process in _processesToMonitor)
+            {
+                // Skip system processes
+                if (process.SessionId == 0)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    process.EnableRaisingEvents = true;
+                    process.Exited += _processToMonitor_Exited;
+                } catch { }
+            }
+        }
+
+        private void _processToMonitor_Exited(object? sender, EventArgs e)
+        {
+            Process process = sender as Process;
+
+            if (process == null) return;
+
+            int exitCode = process.ExitCode;
+
+            if (exitCode == 0)
+            {
+                // We are in a different thread, so
+                // Access the Dispatcher of the main window
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    Main.Opacity = 0;
+
+                    Visibility = Visibility.Visible;
+
+                    _lerpAmount = 0;
+                    _targetOpacity = _maxOpacity;
+                    
+                    Message.Content = $"{process.ProcessName} EXPLODED";
+                });
+            } else
+            {
+                MessageBox.Show("App has crashed");
+            }
         }
 
         private void Update(object sender, EventArgs e)
@@ -49,14 +96,18 @@ namespace you_died
 
         private void UpdateMainOpacity(double deltaTime)
         {
-            _lerpAmount += deltaTime;
+            _lerpAmount += deltaTime / 10;
             Main.Opacity = double.Lerp(Main.Opacity, _targetOpacity, _lerpAmount);
-
 
             if (Main.Opacity <= 0)
             {
-                _lerpAmount = 0;
-                _targetOpacity = _maxOpacity;
+                // Even though opacity is zero, we do this to hide the app from alt-tab
+                Visibility = Visibility.Hidden;
+            }
+
+            // Fix if opacity goes over
+            if (Main.Opacity > _maxOpacity) { 
+                Main.Opacity = _maxOpacity;
             }
 
             if (Main.Opacity >= _maxOpacity)
@@ -71,8 +122,6 @@ namespace you_died
             Debug.WriteLine("Waiting....");
             await Task.Delay(1000);
             Debug.WriteLine("Done waiting!");
-
-            Visibility = Visibility.Visible;
         }
     }
 }
